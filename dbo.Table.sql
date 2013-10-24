5c17741858938bec1a4e352cdfe202ee3ec7a052
Andrew Deacy <andrew-deacy@uiowa.edu>
2013-10-24 15:14:42 -0500

    whats up

---
+++ dbo.Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Recipie] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [SubmittedBy] NVARCHAR(50) NOT NULL, 
+    [Ingredient1] NVARCHAR(50) NULL, 
+    [Ingredient2] NVARCHAR(50) NULL, 
+    [Ingredient3] NVARCHAR(50) NULL, 
+    [Ingredient4] NVARCHAR(50) NULL, 
+    [Ingredient5] NVARCHAR(50) NULL, 
+    [Prep] NVARCHAR(50) NOT NULL, 
+    [Notes] NVARCHAR(50) NULL
+)
